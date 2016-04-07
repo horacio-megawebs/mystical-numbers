761e4a1c4463fe6c7f02e5483924c83457058651
horacio-megawebs <horacio@megawebs.com.ar>
2016-04-07 17:43:04 -0300

    update plugin header

--- mystical-numbers.php
+++ mystical-numbers.php
@@ -1,4 +1,10 @@
 <?php
 /*Plugin Name: Mystical Numbers 
+  Description: This plugin will tell you your daily, monthly and anually positive vibrations.
+  Version: 0.1
+  Author: Horacio Salva
+  Author URI: http://www.megawebs.com.ar/index.php/en/about
+  License: GPL2
+  License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 ?>
