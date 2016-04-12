adebd5e049b3b31b5a459c8ab40a947da62742f6
horacio-megawebs <horacio@megawebs.com.ar>
2016-04-12 11:49:20 -0300

    actualizacion data

--- mystical-numbers.php
+++ mystical-numbers.php
@@ -13,13 +13,13 @@
 ]},
 {"en-years":[
 {"number":"1",
-"keyword","",
-"text-guide","",
-"text-phrase",""},
+"keyword","Beginnings",
+"text-guide","This is the year for new beginnings, for new starts, to new ideas, to any new thing. It is time to plant and to sow seeds. The seeds planted this year will take effect in the following eight years. Remember, the seeds do not grow from one day to another. First they have to germinate and take root. It is until then that begin to grow. Gives your ideas time to take root. Develops a plan for what you want in this cycle of nine years, and started to work in this from now on. I know yourself and moves forward with determination. Take control. Look toward yourself instead of to the associations or unions. During this period, the independence and self-promotion have a strong influence. Lays the Foundation now. Keep yourself busy and things in motion. This is the year to be a pioneer and open gap.",
+"text-phrase","I OPEN NEW PATHS AND START NEW BUSINESSES"},
 {"number","2",
-"keyword","",
-"text-guide","",
-"text-phrase",""}
+"keyword","Cooperate",
+"text-guide","The seeds that plantaste last year are under land ready to germinate. This year deserve some rest and quiet. Make sure you have them. Study and acquire knowledge. Practice diplomacy and the touch. Share and teamwork are the best. I know very cooperative. This year do not force anything. Be patient and wait. What is correct for you, will come. Pay attention to the details. It brings together what you need. Look behind the scenes. The opportunities are already on the way. Think, plans and stay calm. Stay calm. The love is favored. It is a good year for relations and partnerships.",
+"text-phrase","I AM CONFIDENT THAT THE PROCESS OF LIFE WILL DEVELOP IN THE DIVINE ORDER CORRECT"}
 ]
 }
 
